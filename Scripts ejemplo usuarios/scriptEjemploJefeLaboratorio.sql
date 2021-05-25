544817e478b6abb4609cf38edad059e3857c83c6
Jose Avila <joavila@unal.edu.co>
2021-05-24 21:44:19 -0500

    Corregido script ejemplo de usuario Jefe de Laboratorio

--- Scripts ejemplo usuarios/scriptEjemploJefeLaboratorio.sql
+++ Scripts ejemplo usuarios/scriptEjemploJefeLaboratorio.sql
@@ -3,14 +3,14 @@
 */
 USE mydb;
 
+-- Modificar las observaciones de un informe final
+UPDATE mydb.vw_informeFinal_vs_Proyecto
+SET inf_observacionesInforme = 'El ensayo muestra con id=28 se realizó dos veces debido a un fallo en el equipo triaxial'
+WHERE pro_nombreProyecto = 'Contrato de interventoría No. 481 de 2017';
+SELECT * FROM mydb.informeFinal;
+
 -- Intentar obtener información sobre los clientes
 SELECT * FROM mydb.Cliente;
 
 -- Intentar obtener información económica sobre los proyectos
 SELECT * FROM mydb.Proyecto;
-
--- Modificar las observaciones de un informe final
-UPDATE mydb.vw_informeFinal_vs_Proyecto
-SET inf_observacionesInforme = 'UCI covid 19'
-WHERE pro_nombreProyecto = 'Contrato de interventoría No. 481 de 2017';
-SELECT * FROM mydb.informeFinal;
